6b5b60dd1df4fbcf307e12b26c59a0518f7e0308
mjkuhlen <matthewkuhlen@gmail.com>
2023-07-29 16:03:41 -0400

    Update readCSV.ts

--- src/utilities/readCSV.ts
+++ src/utilities/readCSV.ts
@@ -1,13 +1,21 @@
 import fs from 'fs';
-import csv from 'csv-parser';
+import csvParser from 'csv-parser'
 
-export default function readCSV(filePath:string) {
+// Function to read and parse CSV data using streams
+export default function readCSV(filePath: string): Promise<any[]> {
     return new Promise((resolve, reject) => {
-        const results:any = [];
-        fs.createReadStream(filePath)
-            .pipe(csv())
-            .on('data', (data) => results.push(data))
-            .on('end', () => resolve(results))
-            .on('error', (error) => reject(error));
+      const data: any[] = [];
+  
+      fs.createReadStream(filePath)
+        .pipe(csvParser())
+        .on('data', (row: any) => {
+          data.push(row);
+        })
+        .on('end', () => {
+          resolve(data);
+        })
+        .on('error', (error: any) => {
+          reject(error);
+        });
     });
-}
+  }
