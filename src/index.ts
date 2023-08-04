fe58435a1cd095cff1da3be506e9423ce34379d0
mjkuhlen <matthewkuhlen@gmail.com>
2023-08-04 10:05:18 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,7 @@ async function initialize() {
             }
         });
         const date: any = leagues?.current_date;
-        const gamedate = dayjs(date).add(1, 'day').toDate();
+        const gamedate = dayjs(date);
 
         const userRepo = AppDataSource.getRepository(User);
 
@@ -43,17 +43,17 @@ async function initialize() {
         };
 
         const dbDate = await dateRepo.findOneByOrFail({id: 1});
-        const checkDate = dayjs(dbDate.date).toDate();
+        const checkDate = dayjs(dbDate.date);
 
         console.log(`dbDate - ${dbDate.date}`)
         console.log(`gameDate - ${gamedate}`)
         
         if(checkDate !== gamedate) {
-            dbDate.date = gamedate;
+            dbDate.date = gamedate.toDate();
             await dateRepo.save(dbDate);
             const users:User[] = await userRepo.find();
             users.map((user) => {
-                user.gameDate = gamedate;
+                user.gameDate = gamedate.toDate();
                 user.ready = false;
                 userRepo.save(user);
             })
