dd3431cc883e2c13c6dfb69126248f275bfb16df
mjkuhlen <matthewkuhlen@gmail.com>
2023-08-20 22:25:20 -0400

    Update team.ts

--- src/commands/Game/team.ts
+++ src/commands/Game/team.ts
@@ -82,26 +82,10 @@ export default new client.command({
                         select: {
                             first_name: true,
                             last_name: true,
-                            gamesAsWinningPitcher: {
-                                where: {
-                                    game_type: 0,
-                                },
-                                select: {
-                                  game_id: true,
-                                }
-                              },
-                            gamesAsLosingPitcher: {
-                                where: {
-                                    game_type: 0,
-                                },
-                                select: {
-                                  game_id: true,
-                                }
-                            },
                             _count: {
                                 select: {
-                                    gamesAsWinningPitcher: true,
-                                    gamesAsLosingPitcher: true
+                                    gamesAsWinningPitcher: { where: { game_type: 0 }},
+                                    gamesAsLosingPitcher: { where: { game_type: 0 }}
                                 }
                             }
                         },
@@ -110,26 +94,10 @@ export default new client.command({
                         select: {
                             first_name: true,
                             last_name: true,
-                            gamesAsWinningPitcher: {
-                                where: {
-                                    game_type: 0,
-                                },
-                                select: {
-                                  game_id: true,
-                                }
-                              },
-                            gamesAsLosingPitcher: {
-                                where: {
-                                    game_type: 0,
-                                },
-                                select: {
-                                  game_id: true,
-                                }
-                            },
                             _count: {
                                 select: {
-                                    gamesAsWinningPitcher: true,
-                                    gamesAsLosingPitcher: true
+                                    gamesAsWinningPitcher: { where: { game_type: 0 }},
+                                    gamesAsLosingPitcher: { where: { game_type: 0 }}
                                 }
                             }
                         },
@@ -149,7 +117,6 @@ export default new client.command({
             prisma.$disconnect();
             let headlines:any = [];
             games.map((headline: any) => {
-                console.log(headline.winningPitcher.gamesAsWinningPitcher)
                 const date: any = headline.date;
                 const formatDate = date.toISOString().slice(0,19).replace("T", " ");
                 headlines.push(
