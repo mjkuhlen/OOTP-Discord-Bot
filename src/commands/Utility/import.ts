6ddfcc78671232af6c65d43c3eaa79e22caf5c0d
mjkuhlen <matthewkuhlen@gmail.com>
2025-07-25 21:54:12 -0400

    Update import.ts

--- src/commands/Utility/import.ts
+++ src/commands/Utility/import.ts
@@ -41,28 +41,6 @@ export default new client.command({
                         });
                     });
                     importPromises.push(importPromise);
-                } else if (file.endsWith('.csv')) {
-                    const filePath = `${sqlDir}/${file}`;
-                    // Infer table name from file name (e.g., games.csv -> games)
-                    const tableName = file.replace('.csv', '');
-                    console.log(`Queuing import for CSV file: ${filePath}`);
-                    // Build LOAD DATA INFILE SQL command
-                    const sqlCmd = `LOAD DATA LOCAL INFILE '${filePath.replace(/\\/g, '/')}' INTO TABLE ${tableName} FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\\n' IGNORE 1 LINES;`;
-                    // Execute mysql command to import CSV file asynchronously
-                    const importPromise = new Promise<void>((resolve) => {
-                        exec(`mysql --local-infile=1 -h mysql -P 3306 -u ${username} -p${password} ${database} -e "${sqlCmd}"`, (error, stdout, stderr) => {
-                            if (error) {
-                                console.error(`Error importing CSV file: ${filePath}`, error);
-                                if (stderr) console.error(stderr);
-                                importResults.push({ file: filePath, error });
-                            } else {
-                                console.log(`Successfully imported CSV file: ${filePath}`);
-                                importResults.push({ file: filePath });
-                            }
-                            resolve();
-                        });
-                    });
-                    importPromises.push(importPromise);
                 }
             }
             // Wait for all import tasks to complete
